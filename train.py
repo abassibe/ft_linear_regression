8a7aec532c6ac5755601fb76cefc7d2b1670fc6c
Aaron BASSIBEY <abassibe@student.42.fr>
2019-11-05 15:55:06 +0100

    résolution algo

--- train.py
+++ train.py
@@ -13,7 +13,6 @@ import numpy as np
 
 root = Tk()
 root.title("Linear regression")
-path = ''
 
 # Windows creation
 windowWidth = 1200
@@ -36,73 +35,60 @@ previewCanvas.pack(side=LEFT, expand=True, fill=BOTH)
 configLabel = LabelFrame(root, labelanchor='n', text='Configuration')
 configLabel.grid(row=3, column=0, rowspan=4, columnspan=4, sticky='wes')
 
-# CSV path
+# Start button
 pathCSV = StringVar()
-pathCSVButton = Button(configLabel, text='Run Training', command=lambda: selectPath(previewLabel))
+pathCSVButton = Button(configLabel, text='Run Training', command=lambda: start(previewLabel))
 pathCSVButton.grid(row=1, column=1, rowspan=1, columnspan=1, sticky='w')
 
 
 def estimatePrice(theta0, theta1, mileage):
-    return theta0 + (theta1 * mileage)
+    return theta0 + theta1 * mileage
 
 
-def compute_gradients(x, y, m, alpha, iterations):
-    theta = np.zeros((1, 2))
+def computeGradients(x, y,amountData, theta, alpha, iterations):
     for i in range(0, iterations):
-        tmp_theta = np.zeros((1, 2))
-        for j in range(0, m):
-            tmp_theta[0, 0] += (estimatePrice(tmp_theta[0, 0], tmp_theta[0, 1], x[j]) - y[j])
-            tmp_theta[0, 1] += ((estimatePrice(tmp_theta[0, 0], tmp_theta[0, 1], x[j]) - y[j]) * x[j])
-        theta -= (tmp_theta * alpha) / m
-    print(theta)
+        tmpTheta = np.zeros((1, 2))
+        for j in range(0, amountData):
+            tmpTheta[0, 0] += (estimatePrice(theta[0, 0], theta[0, 1], x[j]) - y[j])
+            tmpTheta[0, 1] += ((estimatePrice(theta[0, 0], theta[0, 1], x[j]) - y[j]) * x[j])
+        theta -= (tmpTheta * alpha) /amountData
+    return theta
 
+def standardize(value):
+    return (value - np.mean(value)) / np.std(value)
 
-# Display the path selection window and chooses a file name
-def selectPath(previewLabel):
-    global path
-    x = []
-    y = []
-    theta0 = 0
-    theta1 = 0
-    path = filedialog.askopenfilename(title='Select a file', filetypes=[("CSV", "*.csv")])
-    if not os.path.isfile(path):
-        return
+def destandardize(x, value):
+    return x * np.std(value) + np.mean(value)
+
+# Start trainning
+def start(previewLabel):
+    theta = np.zeros((1, 2))
+    alpha = 0.3
+    data = np.loadtxt("data.csv", dtype = np.longdouble, delimiter = ',', skiprows = 1)
+    if (len(data) < 2):
+        exit()
     fig = Figure(figsize=(13, 7), dpi=96)
     ax = fig.add_subplot()
-    with open(path, newline='') as csvfile:
-        data = csv.reader(csvfile, delimiter=',')
-        for row in data:
-            if row[0].isdigit():
-                x.append(float(row[1]))
-                y.append(float(row[0]))
-            else:
-                axes = fig.axes
-                axes[0].text(0.5, -0.1, row[1], horizontalalignment='center',
-                    transform=ax.transAxes, fontsize=12, color='green')
-                axes[0].text(-0.1, 0.5, row[0], verticalalignment='center',
-                    transform=ax.transAxes, fontsize=12, color='green')
+    axes = fig.axes
+    axes[0].text(0.5, -0.1, 'km', horizontalalignment='center',
+        transform=ax.transAxes, fontsize=12, color='green')
+    axes[0].text(-0.1, 0.5, 'price', verticalalignment='center',
+        transform=ax.transAxes, fontsize=12, color='green')
 
-    m = len(x)
-    x = (x - np.mean(x)) / np.std(x)
-    y = (y - np.mean(y)) / np.std(y)
-    compute_gradients(x, y, m, 0.3, 6)
-    ySomme = 0
-    xSomme = 0
-    squareX = 0
-    bSomme = 0
-    for i in range(0, m):
-        bSomme += (y[i] * x[i])
-        ySomme += y[i]
-        xSomme += x[i]
-        squareX += x[i] * x[i]
-        theta0 += estimatePrice(theta0, theta1, y[i]) - x[i]
-        theta1 += (estimatePrice(theta0, theta1, y[i]) - x[i]) * y[i]
-    b = (m * bSomme) - (ySomme * xSomme) / ((m * squareX) - (xSomme * xSomme))
-    a = (ySomme / m) - (1000 * (xSomme / m))
-    ax.scatter(x, y)
-    l1 = lines.Line2D([0, 1], [0, 1], transform=fig.transFigure, figure=fig)
-    l2 = lines.Line2D([0, 1], [1, 0], transform=fig.transFigure, figure=fig)
-    fig.lines.extend([l1, l2])
+    ax.scatter(data[:, 0], data[:, 1])
+    x = standardize(data[:, 0])
+    y = standardize(data[:, 1])
+    amountData = len(x)
+    theta = computeGradients(x, y, amountData, theta, 0.3, 200)
+    y = estimatePrice(theta[0, 0], theta[0, 1], x)
+    x = destandardize(x, data[:, 0])
+    y = destandardize(y, data[:, 1])
+    a = (y[0] - y[1]) / (x[0] - x[1])
+    b = a * x[0] * -1 + y[0]
+    theta = [b, a]
+
+    ax.plot(x, y)
+
     graph = FigureCanvasTkAgg(fig, master=previewLabel)
     canvas = graph.get_tk_widget()
     canvas.grid(row=0, column=0)
